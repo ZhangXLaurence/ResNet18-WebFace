f045ec94932f0fe7ede9c555cc1969c58def7003
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-03 04:34:21 +0000

    Adding tarfile member sanitization to extractall()

--- bases/DataLoader/LFWDataset.py
+++ bases/DataLoader/LFWDataset.py
@@ -24,7 +24,26 @@ def create_datasets(dataroot, train_val_split=0.9):
         if data_tarball_file not in dataroot_files:
             tarball = download(dataroot, DATASET_TARBALL)
         with tarfile.open(tarball, 'r') as t:
-            t.extractall(dataroot)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(t, dataroot)
 
     images_root = os.path.join(dataroot, 'lfw-deepfunneled')
     names = os.listdir(images_root)
